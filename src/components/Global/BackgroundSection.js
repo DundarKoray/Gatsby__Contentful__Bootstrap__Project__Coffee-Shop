7265515a95fa324e2b577c66854fc456667295e1
Koray Dundar <koraydundar_40@hotmail.com>
2019-08-17 18:39:26 +0300

    added new font-families, added border top, bottom

--- src/components/Global/BackgroundSection.js
+++ src/components/Global/BackgroundSection.js
@@ -4,7 +4,7 @@ import BackgroundImage from "gatsby-background-image"
 const BackgroundSection = ({img, styleClass, title1, title2, children }) => {
     return (
         <BackgroundImage className={styleClass} fluid={img}>
-            <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold">
+            <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold anna-ja-emma">
                 {title1}<br/>{title2}
             </h1>
             {children}
@@ -15,6 +15,7 @@ const BackgroundSection = ({img, styleClass, title1, title2, children }) => {
 export default BackgroundSection;
 
 BackgroundSection.defaultProps = {
-    title: "default title",
+    title1: "default title 1",
+    title2: "default title 2",
     styleClass: "default-background"
 }

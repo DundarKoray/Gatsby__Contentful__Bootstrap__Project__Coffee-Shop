c3155e7ded4bdcf74825349c2d075c4357ddf3db
Koray Dundar <koraydundar_40@hotmail.com>
2019-08-18 15:44:44 +0300

    changed site meta data title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Regular Joe's`,
+    title: `Anna & Emma Kahvi Roastery`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`
   },

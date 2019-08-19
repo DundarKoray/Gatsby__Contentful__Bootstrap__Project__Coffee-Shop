d2471baed5bb0ae9dfb009ecda22ce632555e971
Koray Dundar <koraydundar_40@hotmail.com>
2019-08-20 01:00:47 +0300

    removed png items

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export const query = graphql `
         price
         category
         image{
-          fixed(width:250, height:350){
+          fixed(width:100, height:100){
             ...GatsbyContentfulFixed_tracedSVG
           }
         }

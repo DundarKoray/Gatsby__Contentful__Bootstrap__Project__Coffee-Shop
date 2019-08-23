348ddc662af074940880272a000b20a0594aa28c
Koray Dundar <koraydundar_40@hotmail.com>
2019-08-23 22:16:52 +0300

    removed padding left and right for product card

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -6,7 +6,7 @@ const Product = ({product}) => {
         <div className="col-10 col-sm-8 col-md-6 col-lg-3 mx-auto my-3">
            <div className="card text-center mx-auto d-block py-3">
                <Img fixed={product.image.fixed}/>
-               <div className="card-body py-0">
+               <div className="card-body py-0 px-0">
                    <h6>{product.title}</h6>
                    <h6>€{product.price.toFixed(2)}</h6>
                    <button className="btn btn-yellow mt-3 text-capitalize">
